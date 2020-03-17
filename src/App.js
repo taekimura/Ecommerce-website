315b4c3ab8bf4f49aa70b7d097f2f2503c1b40d2
taekimura <55253319+taekimura@users.noreply.github.com>
2020-03-16 23:31:18 -0700

    update-our-menu-item-to-navigate-withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Route, Switch, Link} from 'react-router-dom';
+
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () =>{
+  return (
+     <div>
+       <h1>HATS PAGE</h1>
+     </div>
+   );
+ };
+
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component ={HomePage}/>
+        <Route path='/hats' component ={HatsPage}/>
+      </Switch>
     </div>
   );
 }
