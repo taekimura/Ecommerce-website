3215d60533aa6d0015b524d9ddb5aa3885cdb87d
taekimura <55253319+taekimura@users.noreply.github.com>
2020-03-20 00:54:05 -0700

    modified-signup-page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import {Route, Switch, Link} from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import signInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component ={HomePage}/>
         <Route path='/shop' component ={ShopPage}/>
+        <Route path='/signin' component ={signInAndSignUp}/>
       </Switch>
     </div>
   );
