78fc448b3271712929ec2f95675632cee32977c9
taekimura <55253319+taekimura@users.noreply.github.com>
2020-03-25 19:55:00 -0700

    complete-sign-up-sign-in-page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch, Link} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 
 import './App.css';
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import signInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component{
@@ -23,10 +23,21 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+          userRef.onSnapshot(snapShot =>{
+            this.setState({
+              currentUser:{
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+          });
+  
+      }
+      this.setState({currentUser: userAuth})
     });
   }
   componentWillUnmount(){
