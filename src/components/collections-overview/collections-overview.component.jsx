30b9d22d90c9083f20a1c349cb4ef2e04ab25866
taekimura <55253319+taekimura@users.noreply.github.com>
2020-04-03 21:31:10 -0700

    modified-overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,20 +4,20 @@ import {createStructuredSelector} from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverView = ({ collections }) => (
     <div className='collections-overview'>
         {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id}{...otherCollectionProps}/>
-            ))}
+            <CollectionPreview key={id}{...otherCollectionProps}/>
+        ))}
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections:selectCollections
+    collections:selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverView);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -23,7 +23,7 @@ const CollectionPage = ({collection}) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionID)(state)
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 
 export default connect(mapStateToProps)(CollectionPage);
