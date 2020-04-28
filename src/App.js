436f9005e42311c8e558f6eca0a3fafb9da9a21e
taekimura <55253319+taekimura@users.noreply.github.com>
2020-04-28 05:32:34 -0700

    converted to redux-saga concept

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-
-import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 
 class App extends React.Component{
@@ -22,20 +19,19 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    const {setCurrentUser} = this.props;
-    auth.onAuthStateChanged(async userAuth => {
-      if (userAuth){
-          const userRef = await createUserProfileDocument(userAuth);
+    // auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth){
+    //       const userRef = await createUserProfileDocument(userAuth);
 
-          userRef.onSnapshot(snapShot =>{
-            setCurrentUser({
-                id: snapShot.id,
-                ...snapShot.data()
-            });
-          });
-        }
-      setCurrentUser(userAuth);
-    },error => console.log(error));
+    //       userRef.onSnapshot(snapShot =>{
+    //         setCurrentUser({
+    //             id: snapShot.id,
+    //             ...snapShot.data()
+    //         });
+    //       });
+    //     }
+    //   setCurrentUser(userAuth);
+    // },error => console.log(error));
   }
   componentWillUnmount(){
     this.unsubscribeFromAuth();
@@ -68,11 +64,4 @@ class App extends React.Component{
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
-const mapDispatchToProps = dispatch =>({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-  )(App);
+export default connect(mapStateToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
-const middleware = [thunk];
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development'){
     middleware.push(logger);
@@ -14,6 +17,7 @@ if(process.env.NODE_ENV === 'development'){
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
 
 export default {store, persistor};
